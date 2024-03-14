76033c2c6a110f8225da84e50c40af77563c1b36
huangyul <1103221645@qq.com>
2024-03-14 21:13:48 +0800

    feat: gorm

--- basic/06_gorm/gorm_test.go
+++ basic/06_gorm/gorm_test.go
@@ -1,6 +1,7 @@
 package _6_gorm
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -12,7 +13,14 @@ import (
 
 type User struct {
 	gorm.Model
-	Name string
+	Name  string
+	Cards []Card `gorm:"foreignKey:UserID"`
+}
+
+type Card struct {
+	gorm.Model
+	Name   string
+	UserID uint
 }
 
 func Test_Gorm(t *testing.T) {
@@ -35,30 +43,57 @@ func Test_Gorm(t *testing.T) {
 	}
 
 	// 数据库迁移
-	err = db.AutoMigrate(&User{})
+	err = db.AutoMigrate(&User{}, &Card{})
 	if err != nil {
 		panic(err)
 	}
-
-	// 创建数据
-	db.Create(&User{Name: "name"})
-
-	// 批量创建数据
-	//var users = []User{{Name: "name1"}, {Name: "name2"}}
-	//db.Create(&users)
-
+	//
+	//db.Create(&User{Name: "user"})
+	//db.Create(&Card{
+	//	Name:   "card1",
+	//	UserID: 1,
+	//})
 	var user User
-	var users []User
-	// 根据主键排序，获取第一条
-	db.First(&user)
-	// 指定主键
-	db.First(&user, 1)
-	// 获取数据库第一条数据
-	db.Take(&user)
-	// 获取主键降序最后一条
-	db.Last(&user)
-	// 获取全部数据
-	db.Find(&users)
+	db.Preload("Cards").First(&user)
+	fmt.Println(user.Cards)
 }
 
-// 4-10
+// 创建数据
+//db.Create(&User{Name: "name"})
+
+// 批量创建数据
+//var users = []User{{Name: "name1"}, {Name: "name2"}}
+//db.Create(&users)
+
+//var user User
+//var users []User
+//// 根据主键排序，获取第一条
+//db.First(&user)
+//// 指定主键
+//db.First(&user, 1)
+//// 获取数据库第一条数据
+//db.Take(&user)
+//// 获取主键降序最后一条
+//db.Last(&user)
+//// 获取全部数据
+//db.Find(&users)
+
+// where 查询
+//var user User
+//// 拼接
+//db.Where("name = ?", "name").First(&user)
+//// 使用struct
+//db.Where(&User{Name: "name"}).First(&user)
+//// 使用map
+//db.Where(map[string]any{"name": "name"}).First(&user)
+//
+//fmt.Println(user)
+
+// 更新
+//user := &User{
+//	Name: "new name",
+//}
+// 如果有主键就更新，没有主键就新增
+//db.Save(&user)
+// 只更新特定字段
+//db.Model(User{}).Where("name = ?", "test").Limit(1).Update("name", "huang")
